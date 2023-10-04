1d7ac608134dd6e95464956cdb2c826c554d32c2
shiva2013-tech <133153012+shiva2013-tech@users.noreply.github.com>
2023-10-04 17:29:20 +0530

    Account Owner Server

---
+++ src/app/owner/owner.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OwnerRoutingModule } from './owner-routing.module';
+import { OwnerListComponent } from './owner-list/owner-list.component';
+import { OwnerDetailsComponent } from './owner-details/owner-details.component';
+import { OwnerAccountsComponent } from './owner-details/owner-accounts/owner-accounts.component';
+import { SharedModule } from '../shared/shared.module';
+import { OwnerCreateComponent } from './owner-create/owner-create.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { DatePipe } from '@angular/common';
+import { AppComponent } from '../app.component';
+import { OwnerUpdateComponent } from './owner-update/owner-update.component';
+import { OwnerDeleteComponent } from './owner-delete/owner-delete.component';
+
+@NgModule({
+  declarations: [
+    OwnerListComponent,
+    OwnerDetailsComponent,
+    OwnerAccountsComponent,
+    OwnerCreateComponent,
+    OwnerUpdateComponent,
+    OwnerDeleteComponent
+  ],
+  imports: [
+    CommonModule,
+    OwnerRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot()
+  ],
+  providers: [DatePipe],
+  bootstrap: [AppComponent]
+})
+export class OwnerModule { }
